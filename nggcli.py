254e9c5a95fe1454d675117ecc86f876b94be848
azulf <azulf@github.com>
2025-01-17 05:23:06 +0700

    Fix

--- nggcli.py
+++ nggcli.py
@@ -1,4 +1,57 @@
 import random
 import os
 import sys
+import argparse
 
+def main() :
+    if len(sys.argv) < 2:
+        print("Selamat datang di NGG (Number Guessing Game)")
+        print("Number Guessing Game adalah permainan menggunakan CLI")
+        print("Kamu akan disuruh untuk menebak angka yang dipikirkan oleh program")
+        print("Angka yang diberikan berkisar antara 1 - 100")
+        print("Kamu memiliki 5 kesempatan untuk menebak")
+    angka = saving_number()
+    # print(angka) # for debug
+    diffy = input("""Pilih Kesulitan 
+          1. Hard   = 3  chance
+          2. Medium = 5  chance
+          3. Easy   = 10 chance
+          """)
+    kesempatan = difficulty_option(diffy)
+    angka_tebakan = int(input("Masukkan Angka : "))
+
+    while angka != angka_tebakan and kesempatan > 1 :
+        kesempatan -= 1
+        print("Salah ! ")
+        print(f"Kesempatan Kamu tinggal : {kesempatan}")
+        angka_tebakan = int(input("Masukkan Angka : "))
+    if angka == angka_tebakan :
+        print("Congrats kamu berhasil menebaknya ! ")
+    else :
+        print("Kamu kalah kesempatan kamu sudah habis !")
+        print(f"Angka yang harus ditebak adalah {angka}")
+    
+    
+# saving number that should be guessed
+def saving_number() :
+    number = random.randint(1, 100)
+    return number
+
+# option for difficulty 
+def difficulty_option(diffy) :
+    hard = 3
+    medium = 5
+    easy = 10
+
+    if diffy == 'hard' or 1 :
+        diffy = hard
+    elif diffy == 'medium' or 2 :
+        diffy = medium
+    elif diffy == 'easy' or 3 :
+        diffy = easy
+    return diffy
+
+if __name__ == '__main__':
+    main()
+    
+       
