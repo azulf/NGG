c782d8347e52593a4c24948de704566983389f3e
azulf <azulf@github.com>
2025-01-17 05:34:46 +0700

    fixing and adding difficulty

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # NGG
+https://github.com/azulf/NGG
+https://roadmap.sh/projects/number-guessing-game
--- nggcli.py
+++ nggcli.py
@@ -12,16 +12,20 @@ def main() :
         print("Kamu memiliki 5 kesempatan untuk menebak")
     angka = saving_number()
     # print(angka) # for debug
-    diffy = input("""Pilih Kesulitan 
+    diffy = int(input("""Pilih Kesulitan 
           1. Hard   = 3  chance
           2. Medium = 5  chance
           3. Easy   = 10 chance
-          """)
+          """))
     kesempatan = difficulty_option(diffy)
     angka_tebakan = int(input("Masukkan Angka : "))
 
     while angka != angka_tebakan and kesempatan > 1 :
         kesempatan -= 1
+        if angka > angka_tebakan :
+            print("Angka kamu lebih kecil !")
+        elif angka < angka_tebakan :
+            print("Angka kamu lebih besar !")
         print("Salah ! ")
         print(f"Kesempatan Kamu tinggal : {kesempatan}")
         angka_tebakan = int(input("Masukkan Angka : "))
@@ -43,12 +47,13 @@ def difficulty_option(diffy) :
     medium = 5
     easy = 10
 
-    if diffy == 'hard' or 1 :
+    if  diffy == 'hard' or diffy == 1 :
         diffy = hard
-    elif diffy == 'medium' or 2 :
-        diffy = medium
-    elif diffy == 'easy' or 3 :
-        diffy = easy
+    elif diffy == 'medium' or diffy == 2 :
+         diffy = medium
+    elif diffy == 'easy' or diffy == 3 :
+         diffy = easy
+
     return diffy
 
 if __name__ == '__main__':
